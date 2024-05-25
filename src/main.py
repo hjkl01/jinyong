ff52b3bc2878f6c1d2d487369dd8f372f6c9c74b
ycm76229 <ycm76229@gmail.com>
2024-05-25 09:16:56 +0800

    delete useless

--- src/main.py
+++ src/main.py
@@ -5,72 +5,69 @@ import os
 from flask import Flask, render_template, jsonify, send_from_directory
 from common.model import Jinyong as jy
 from loguru import logger
-logger.add("logs/%s.log" % __file__.rstrip('.py'), format="{time:MM-DD HH:mm:ss} {level} {message}")
+
+logger.add("logs/%s.log" % __file__.rstrip(".py"), format="{time:MM-DD HH:mm:ss} {level} {message}")
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     _str = '<html>  <meta http-equiv="refresh" content="0;url=../home/">  </html> '
     return _str
 
 
-@app.route('/home/')
+@app.route("/home/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
-@app.route('/ludingji/')
-@app.route('/ludingji/<page>')
+@app.route("/ludingji/")
+@app.route("/ludingji/<page>")
 def ludingji(page=1):
     if int(page) > 52:
-        result = 'page num is wrong !'
+        result = "page num is wrong !"
         return jsonify(result)
     qu = jy.select().where(jy.id == page).get()
     titles = [t.title for t in jy.select()]
-    result = [x for x in qu.content.split('br')]
-    return render_template('ludingji.html', content=result, head=qu.name, titles=titles, title=qu.title)
+    result = [x for x in qu.content.split("br")]
+    return render_template("ludingji.html", content=result, head=qu.name, titles=titles, title=qu.title)
 
 
-@app.route('/<path:_file>')
+@app.route("/<path:_file>")
 def favicon(_file):
-    return send_from_directory(os.path.join(app.root_path, 'static'), _file)
+    return send_from_directory(os.path.join(app.root_path, "static"), _file)
 
 
-@app.route('/google/')
+@app.route("/google/")
 def google():
-    return render_template('google.html')
+    return render_template("google.html")
 
 
-@app.route('/v/')
+@app.route("/v/")
 def list_movies():
     import os
-    files = os.listdir('video')
-    return render_template('listdir.html', data=files)
+
+    files = os.listdir("video")
+    return render_template("listdir.html", data=files)
 
 
-@app.route('/v/<filename>')
+@app.route("/v/<filename>")
 def get_file(filename):
-    path = 'video'
+    path = "video"
     return send_from_directory(path, filename)
 
 
-@app.route('/secret/')
-def _srcret():
-    return render_template('secret.html')
-
-
-@app.route('/test/')
+@app.route("/test/")
 def test():
-    return render_template('test.html')
+    return render_template("test.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host="0.0.0.0", port=8000)
     # app.run()
